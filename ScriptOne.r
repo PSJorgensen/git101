9cbf613f1526c7f6ff5fb751656fd037cd6a091f
PSJorgensen <psjoergensen@gmail.com>
2015-02-12 13:53:03 +0100

    This is my third commit, the second to this script. This commit includes a random walk.

--- ScriptOne.r
+++ ScriptOne.r
@@ -1,2 +1,20 @@
 ###############   THIS IS MY FIRST SCRIPT USING R STUDIO, GIT AND GITHUB
 
+## first executable command - the random walk, which is what this currently feels like
+
+#setting parameters
+set.seed(235)
+sim.length<-100
+n.vec<-rep(NA,sim.length)
+n0<-rnorm(1)  
+
+#random walk loop
+for(i in 1:sim.length){
+if(i==1){ni<-n0}
+if(i>1){ni<-n.vec[i-1]}
+n.vec[i]<-ni+rnorm(1)  
+}
+
+# plotting results
+plot(n.vec,type="l")
+
