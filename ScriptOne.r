9c2b7860922cf0f1ad68152f8e739f8723e8eae3
PSJorgensen <psjoergensen@gmail.com>
2015-02-12 15:36:14 +0100

    compiling function and then running simulations

--- ScriptOne.r
+++ ScriptOne.r
@@ -3,12 +3,13 @@
 ## first executable command - the random walk, which is what this currently feels like
 
 #setting parameters
-set.seed(233)
-sim.number<-100
-sim.length<-100
-n.vec<-rep(NA,sim.length)
-n0<-rnorm(1)  
 
+rand.walk<-function(sim.number,sim.length){
+
+#creating initital objects
+n0<-rnorm(1)  
+n.vec<-rep(NA,sim.length)
+  
 #simulation loop
 for(j in 1:sim.number){
 #random walk loop
@@ -21,6 +22,11 @@ if(j == 1){n.sims<-n.vec}
 if(j > 1){n.sims<-cbind(n.sims,n.vec)}
 }
 
+n.sims.df<-as.data.frame(n.sims)
+names(n.sims.df)<-seq(1:sim.number)
+
+
+
 #doing summary statistics of the simulations
 summary.df<-as.data.frame(t(
   apply(
@@ -28,18 +34,39 @@ summary.df<-as.data.frame(t(
     )
     ))
 
+names(summary.df)<-c("mean","sd")
+
 summary.df[,"n.plus.sd"]<-summary.df[,1]+summary.df[,2]
 summary.df[,"n.minus.sd"]<-summary.df[,1]-summary.df[,2]
 
-# initiating plotting of summary results
-plot(summary.df[,1], type = "l", ylim = c(min(summary.df),max(summary.df)),
-     xlab = "time", ylab = "n")
+
+# initiating printing of plot of summary results
+plot(summary.df[,"mean"],
+     type = "l",
+     ylim = c(min(summary.df),max(summary.df)),
+     xlab = "time",
+     ylab = "n"
+     )
 
 #plotting standard deviation
-polygon(x = c(1:sim.number, sim.number:1),
+polygon(x = c(1:sim.length, sim.length:1),
         y = c(summary.df[,"n.minus.sd"], rev(summary.df[,"n.plus.sd"])), 
-        col = "grey70")
+        col = "grey70"
         )
 
 #plotting mean
 lines(summary.df[,1])
+
+
+## creating a list to return objects
+return.object<-list()
+return.object[["summary"]]<-summary.df
+return.object[["sims"]]<-n.sims.df
+
+## returning objects
+return(return.object)
+
+}
+
+set.seed(234)
+output<-rand.walk(sim.number=500,sim.length=200)
